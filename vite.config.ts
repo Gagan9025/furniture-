4d7ce87e970134f00a5f3124ee18feb595f9428e
Gagan <ggagan92364@gmail.com>
2026-02-02 23:01:40 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: "../client" // copies to build/client/ directory
+        }
+      ]
+    })
+  ],
 });
